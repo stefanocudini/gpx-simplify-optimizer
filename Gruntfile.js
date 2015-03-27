dc9765569968bb2d33a7f05de952d9ef36f1b735
Wilkins <thibault@taillandier.name>
2015-03-27 11:47:00 +0100

    Vendor - moving dependencies in vendor/

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,15 +24,15 @@ grunt.initConfig({
 	uglify: {
 		dist: {
 			files: {
-				'dist/jquery.cookie.min.js': ['jquery-cookie/jquery.cookie.js'],
-				'dist/L.Control.Sidebar.min.js': ['leaflet-sidebar/src/L.Control.Sidebar.js'],
-				'dist/togeojson.min.js': ['togeojson/togeojson.js'],
-				'dist/togpx.min.js': ['togpx/togpx.js'],
-				'dist/pretty-data.min.js': ['pretty-data/index.js'],
-				'dist/tokml.min.js': ['tokml/tokml.js'],
-				'dist/geojson-to-path.min.js': ['geojson-to-path/geojson-to-path.js'],
-				'dist/leaflet.filelayer.min.js': ['Leaflet.FileLayer/leaflet.filelayer.js'],
-				'dist/FileSaver.min.js': ['FileSaver.js/FileSaver.js'],
+				'dist/jquery.cookie.min.js': ['vendor/jquery-cookie/jquery.cookie.js'],
+				'dist/L.Control.Sidebar.min.js': ['vendor/leaflet-sidebar/src/L.Control.Sidebar.js'],
+				'dist/togeojson.min.js': ['vendor/togeojson/togeojson.js'],
+				'dist/togpx.min.js': ['vendor/togpx/togpx.js'],
+				'dist/pretty-data.min.js': ['vendor/pretty-data/index.js'],
+				'dist/tokml.min.js': ['vendor/tokml/tokml.js'],
+				'dist/geojson-to-path.min.js': ['vendor/geojson-to-path/geojson-to-path.js'],
+				'dist/leaflet.filelayer.min.js': ['vendor/Leaflet.FileLayer/leaflet.filelayer.js'],
+				'dist/FileSaver.min.js': ['vendor/FileSaver.js/FileSaver.js'],
 				'dist/formats.min.js': ['formats.js', 'formats/*'],
 				'dist/main.min.js': ['main.js']
 			}
@@ -57,8 +57,8 @@ grunt.initConfig({
 				'dist/geojson-to-path.min.js',
 				'dist/leaflet.filelayer.min.js',
 				'dist/FileSaver.min.js',
-				'simplify-geometry/simplifygeometry-0.0.1.min.js',
-				'bootstrap-slider/dist/bootstrap-slider.min.js',
+				'vendor/simplify-geometry/simplifygeometry-0.0.1.min.js',
+				'vendor/bootstrap-slider/dist/bootstrap-slider.min.js',
 				//'crumble/js/jquery.grumble.min.js',
 				//'crumble/js/jquery.crumble.min.js',				
 				'social.js',
@@ -71,8 +71,8 @@ grunt.initConfig({
 	cssmin: {
 		combine: {
 			src: [
-				'bootstrap-slider/dist/css/bootstrap-slider.min.css',
-				'leaflet-sidebar/src/L.Control.Sidebar.css',
+				'vendor/bootstrap-slider/dist/css/bootstrap-slider.min.css',
+				'vendor/leaflet-sidebar/src/L.Control.Sidebar.css',
 				//'crumble/css/grumble.min.css',
 				//'crumble/css/crumble.css',				
 				'style.css'
